d15bd44720a2df771432b7c51942f9e6cb5ee434
asmajawaid <arshadasma45678@gmail.com>
2024-08-16 19:27:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+//BMI calculator in typescript
+import inquirer from "inquirer";
+const questions = [
+    {
+        type: "number",
+        name: "weight",
+        message: "weight(kg)"
+    },
+    {
+        type: "number",
+        name: "height",
+        message: "height(m)"
+    }
+];
+const calculateBMI = (weight, height) => weight / (height * height);
+const main = async () => {
+    const { weight, height } = await inquirer.prompt(questions);
+    console.log(`chalk.blue(BMI:${calculateBMI(weight, height).toFixed(2)})`);
+};
+main();
+// let weight = 70;
+// let height = 5.4;
+// let bmi = weight / height * height 
+// console.log (`asma your BMI is ${bmi}`);
